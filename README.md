04f7775b28a0d312c667e499a4b116728c864b3c
Andreas Hilbig <ahilbig@me.com>
2018-05-02 23:34:38 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Installation
+> `npm install --save https://github.com/ahilbig/types-threex.domevents.git`
+
+# Summary
+This package contains type definitions for threex.js (http://www.threejsgames.com/extensions ).
+
+# Details
+Subprojects currently in scope: http://www.threejsgames.com/extensions/#threex.domevents
+
+Additional Details
+ * Last updated: Wed, 02 May 2018 23:40:54 GMT
+ * Dependencies: 
+ * Global values: THREEx
+
+# Credits
+These definitions were written by Andreas Hilbig <https://github.com/ahilbig>
---
+++ index.d.ts
@@ -0,0 +1,19 @@
+// THREEX.Domevents
+import {Camera, Object3D} from "three";
+
+export declare namespace THREEx {
+  export class DomEvents {
+
+    constructor(camera: Camera, domElement?: HTMLElement);
+
+    object: Camera;
+    domElement: HTMLElement | HTMLDocument;
+
+    // add an event listener for this callback
+    addEventListener(obect: Object3D, type: string, listener: (event: Event) => void, withBoundingBox: boolean): void;
+
+    // remove an event listener for this callback
+    removeEventListener(obect: Object3D, type: string, listener: (event: Event) => void, withBoundingBox: boolean): void;
+
+  }
+}
