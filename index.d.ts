a15729fbcdeddeaef96998388b1db5c3137f3643
Andreas Hilbig <ahilbig@users.noreply.github.com>
2018-05-04 12:26:03 +0200

    Reflected default export mechanism from three.domevents fork

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,17 @@
 // THREEX.Domevents
 import {Camera, Object3D} from "three";
 
-export declare namespace THREEx {
-  export class DomEvents {
+export default class DomEvents {
 
-    constructor(camera: Camera, domElement?: HTMLElement);
+  constructor(camera: Camera, domElement?: HTMLElement);
 
-    object: Camera;
-    domElement: HTMLElement | HTMLDocument;
+  object: Camera;
+  domElement: HTMLElement | HTMLDocument;
 
-    // add an event listener for this callback
-    addEventListener(obect: Object3D, type: string, listener: (event: Event) => void, withBoundingBox: boolean): void;
+  // add an event listener for this callback
+  addEventListener(obect: Object3D, type: string, listener: (event: Event) => void, withBoundingBox: boolean): void;
 
-    // remove an event listener for this callback
-    removeEventListener(obect: Object3D, type: string, listener: (event: Event) => void, withBoundingBox: boolean): void;
+  // remove an event listener for this callback
+  removeEventListener(obect: Object3D, type: string, listener: (event: Event) => void, withBoundingBox: boolean): void;
 
-  }
 }
